0f410d88ee0b5f213ce659cb0388d9db3a91813b
Victor Ivan <viictor.ivan@gmail.com>
2021-10-03 19:41:39 -0300

    feat: adding a db for heroku

--- models/db_model.php
+++ models/db_model.php
@@ -2,10 +2,11 @@
 
 class Database
 {
-    private $serverName = "localhost";
-    private $dbUserName = "root";
-    private $dbPWD  = "";
-    private $dbName = "test";
+    private $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    private $serverName = $this->cleardb_url['host'] | "localhost";
+    private $dbUserName = $this->cleardb_url['user'] | "root";
+    private $dbPWD  = $this->cleardb_url['pass'] | "";
+    private $dbName = substr($this->cleardb_url["path"],1) | "test";
     
     public function connect () {
         $str = 'mysql:host='.$this->serverName.';dbname='.$this->dbName;
