2fc2439efecb696d542a2135316c73f7d681bd97
Victor Ivan <viictor.ivan@gmail.com>
2021-10-03 19:48:48 -0300

    feat: adding a db for heroku

--- models/db_model.php
+++ models/db_model.php
@@ -2,16 +2,17 @@
 
 class Database
 {
-    private $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    private $serverName = $this->cleardb_url['host'];
-    private $dbUserName = $this->cleardb_url['user'];
-    private $dbPWD  = $this->cleardb_url['pass'];
-    private $dbName = substr($this->cleardb_url["path"],1);
-    
     public function connect () {
-        $str = 'mysql:host='.$this->serverName.';dbname='.$this->dbName;
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $serverName = $cleardb_url['host'];
+        $dbUserName = $cleardb_url['user'];
+        $dbPWD  = $cleardb_url['pass'];
+        $dbName = substr($cleardb_url["path"],1);
+
+        
+        $str = 'mysql:host='.$serverName.';dbname='.$dbName;
         try{
-            $conn = new PDO ($str, $this->dbUserName, $this->dbPWD);
+            $conn = new PDO ($str, $dbUserName, $dbPWD);
             return $conn;
         }catch (PDOException $e) {
             echo "Error!: " . $e->getMessage() . "<br/>";
